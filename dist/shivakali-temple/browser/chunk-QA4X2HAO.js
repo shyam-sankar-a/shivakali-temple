1ff12ca0d78333b4879018d9915bdbece0d67faf
shyam-sankar-a <shyam.sankar.jshobby@gmail.com>
2026-05-13 13:09:10 +0900

    removed backend nodemodules and dist folder

--- dist/shivakali-temple/browser/chunk-QA4X2HAO.js
+++
@@ -1,1 +0,0 @@
-var t={production:!0,apiUrl:"/api",stripePublishableKey:"pk_test_51TWRlqBxl5hSibyxtbT74U8eEtLmczpjddfv4OUhPMwO86jqb6eYA9cwVjVSGndkFNWEg4tVsJUX1AuVEKg0uigX00qqnv0Edd"};export{t as a};
